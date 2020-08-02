3a222e0ce771c9a45e56791c09f8ebb4f98f8491
Arun Swaminathan <swaminathaar@vmware.com>
2020-08-02 14:06:36 -0700

    Formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,65 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2020': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 11,
-  },
-  'rules': {
-    'indent': 4,
-  },
+    env: {
+        node: true,
+        browser: true,
+        commonjs: true,
+        es2020: true,
+        jest: true,
+    },
+    extends: ["eslint:recommended", "google"],
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["off", "double"],
+        "semi": ["error", "always"],
+        "no-mixed-spaces-and-tabs": ["error"],
+        "no-useless-escape": ["warn"],
+        "no-console": ["off"],
+        "no-unused-vars": ["error"],
+        "no-redeclare": ["error"],
+        "no-octal": ["error"],
+        "no-array-constructor": ["error"],
+        "no-new-object": ["error"],
+        "arrow-spacing": ["error"],
+        "constructor-super": ["error"],
+        "no-new-require": ["error"],
+        "no-shadow-restricted-names": ["error"],
+        "no-useless-catch": ["error"],
+        "no-useless-call": ["error"],
+        "no-mixed-operators": [
+            "error",
+            {
+                groups: [
+                    ["&", "|", "^", "~", "<<", ">>", ">>>"],
+                    ["&&", "||"],
+                ],
+            },
+        ],
+        "no-return-await": ["error"],
+        "no-return-assign": ["error", "except-parens"],
+        "no-empty": ["warn"],
+        "no-trailing-spaces": ["error"],
+        "no-new-wrappers": ["error"],
+        "no-lone-blocks": ["error"],
+        "no-async-promise-executor": ["error"],
+        "no-self-compare": ["error"],
+        "no-sequences": ["error"],
+        "no-useless-rename": ["error"],
+        "no-useless-concat": ["error"],
+        "no-useless-computed-key": ["error"],
+        "callback-return": [
+            "error",
+            ["callback", "cb", "next", "done", "reject", "resolve"],
+        ],
+        "no-use-before-define": [
+            "error",
+            { functions: false, classes: true, variables: true },
+        ],
+        "no-restricted-modules": ["error", "crypto"],
+        'object-curly-spacing': ['error', 'always'],
+
+    },
 };
--- blm.js
+++ blm.js
@@ -1,6 +1,9 @@
 const { program } = require("commander");
 const { BLM } = require("./lib/blm");
 
+/**
+ * Initialize command line options
+ */
 function initialise() {
     program.version("0.0.1");
     program
@@ -13,26 +16,38 @@ function initialise() {
         .option("-s, --summary", "Summary", true)
         .parse(process.argv);
 
-    process.on("SIGINT", function () {
+    process.on("SIGINT", function() {
         console.log("Terminating...");
         process.exit();
     });
 }
 
+/**
+ * Print Summary of inappropriate words in file/directory
+ */
 async function printSummary() {
     await BLM.use(program.wordsFile).with(program.path).printSummary();
 }
 
+/**
+ * Dump JSON of inappropriate words in file/directory along with line/lineno.
+ */
 async function dumpJSON() {
     await BLM.use(program.wordsFile)
         .with(program.path)
         .dumpJSON(program.verbose);
 }
 
+/**
+ * Replace inappropriate words with words present in dictionary.
+ */
 async function replaceAllWords() {
     await BLM.use(program.wordsFile).withDirectory(program.path).replace();
 }
 
+/**
+ * Replace inappropriate words interactively
+ */
 async function replaceAllWordsInteractive() {
     await BLM.use(program.wordsFile).with(program.path).replaceInteractive();
 }
