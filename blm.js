6332213601e197ba3eda599328dc4a706ef3f33d
Arun Swaminathan <swaminathaar@vmware.com>
2020-08-02 01:47:35 -0700

    Refactor codebase and add tests

--- blm.js
+++ blm.js
@@ -1,18 +1,12 @@
 const { program } = require("commander");
-const { promisify } = require("util");
-const prompt = require("prompt");
-const _ = require("lodash");
-
 const { BLM } = require("./lib/blm");
-
-const getPromptAsync = promisify(prompt.get);
 
 function initialise() {
     program.version("0.0.1");
     program
+        .option("-i, --interactive", "Replace interactively", false)
         .option("-j, --json", "Dump changes as json", false)
-        .option("-f, --file <file>", "File to transform", null)
-        .option("-d, --directory <dir>", "Directory to traverse", "./")
+        .option("-p, --path <path>", "Path (File/Dir) to transform", null)
         .option("-w, --wordsFile <file>", "Words File")
         .option("-r, --replaceAll", "Replace all instances", false)
         .option("-v, --verbose", "Verbosity of JSON output", false)
@@ -25,63 +19,42 @@ function initialise() {
     });
 }
 
+async function printSummary() {
+    await BLM.use(program.wordsFile).with(program.path).printSummary();
+}
+
+async function dumpJSON() {
+    await BLM.use(program.wordsFile)
+        .with(program.path)
+        .dumpJSON(program.verbose);
+}
+
+async function replaceAllWords() {
+    await BLM.use(program.wordsFile).withDirectory(program.path).replace();
+}
+
+async function replaceAllWordsInteractive() {
+    await BLM.use(program.wordsFile).with(program.path).replaceInteractive();
+}
+
 /**
  * Main driver function
  */
 async function main() {
     initialise();
-
-    let blm = new BLM();
-    blm = await blm.use(program.wordsFile);
-
-    let fPaths = [];
-    if (_.isNil(program.file_)) {
-        blm = await blm.withDirectory(program.directory);
-    } else {
-        blm = blm.withFile(program.file);
-    }
-
-    if (program.summary) {
-        await blm.printSummary(fPaths);
-        return;
-    }
-
     if (program.json) {
-        await blm.dumpJSON(fPaths, program.verbose);
-        return;
+        return dumpJSON();
     }
 
-    const property = {
-        name: "yesno",
-        message: "Do you want replace words in file?",
-        validator: /y[es]*|n[o]?/,
-        warning: "Must respond yes or no",
-        default: "no",
-    };
-
-    if (program.replaceAll === false) {
-        prompt.start({ noHandleSIGINT: true });
-
-        for (const fPath of fPaths) {
-            console.log(`\nProcessing file ${fPath}`);
-            try {
-                const result = await getPromptAsync(property);
-            } catch (err) {
-                console.log(`\nTerminating... ${err}`);
-                return;
-            }
-
-            if (/y[es]*/.test(result.yesno)) {
-                await blm.replace(fPath, wordsFile);
-            } else {
-                console.log("\nIgnoring...");
-            }
-        }
-    } else {
-        for (const fPath of fPaths) {
-            await blm.replace(fPath, program.replaceAll);
+    if (program.replaceAll === true) {
+        if (program.interactive === true) {
+            return replaceAllWordsInteractive();
+        } else {
+            return replaceAllWords();
         }
     }
+
+    return printSummary();
 }
 
 main();
